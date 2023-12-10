8ecb86a0122d936a056cecc1cd39a4138e484691
Aarnow <arnaudfgr.dev@gmail.com>
2023-12-10 12:20:51 +0100

    Initial Project Files

---
+++ Main.cs
@@ -0,0 +1,44 @@
+﻿using Life;
+using Life.Network;
+using Life.UI;
+using UnityEngine;
+
+namespace MyMenu
+{
+    public class Main : Plugin
+    {
+        private string version = "0.1.0";
+        public static UIPanel panel = new UIPanel("MyMenu", UIPanel.PanelType.Tab).SetTitle("MyMenu");
+        public Main(IGameAPI api):base(api)
+        {
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Debug.Log($"Plugin \"MyMenu\" initialisé avec succès.");
+        }
+
+        public override void OnPlayerInput(Player player, KeyCode keyCode, bool onUI)
+        {
+            base.OnPlayerInput(player, keyCode, onUI);
+
+            if(keyCode == KeyCode.Y)
+            {
+                UIPanel playerPanel = new UIPanel("MyMenu", UIPanel.PanelType.Tab).SetTitle($"MyMenu v{version}");
+
+                foreach (UITabLine line in panel.lines)
+                {
+                    playerPanel.AddTabLine(line.name, line.action);
+                }
+                
+                playerPanel.AddButton("Sélectionner", ui =>
+                {
+                    ui.lines[ui.selectedTab].action.Invoke(ui);
+                });
+                playerPanel.AddButton("Fermer", ui => player.ClosePanel(playerPanel));
+                player.ShowPanelUI(playerPanel);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Novalife Plugin MyMenu
+
+Ce plugin Novalife MyMenu fournit la base d'un menu sur lequel les différents développeurs de plugins peuvent implémenter leurs sections respectives.
+L'objectif est d'utiliser le même menu afin d'éviter les conflits potentiels entre nos plugins.
+
+## Table des Matières
+
+- [Installation](#installation)
+- [Utilisation](#utilisation)
+- [Fonctionnalités](#fonctionnalités)
+- [Droits de Propriété Intellectuelle](#droits-de-propriété-intellectuelle)
+
+## Installation
+
+1. Téléchargez le fichier `MyMenu.dll` depuis la page des releases de ce dépôt. 
+2. Ajoutez le fichier `MyMenu.dll` dans le dossier des plugins de votre serveur Novalife.
+
+## Utilisation
+
+Le Plugin MyMenu instaure la base d'un menu commun.  
+Pour l'utiliser, suivez ces étapes :
+
+1. Intégrez le Plugin MyMenu.dll en tant que référence dans votre projet Novalife.
+
+## Fonctionnalités
+
+Exemples à venir...
+
+```csharp
+//exemple
+```
+
+## Droits de Propriété Intellectuelle
+
+Je vous demande simplement de respecter le temps que j'ai mis dans ce plugin.  
+Merci de ne pas vous approprier le plugin, de ne pas le copier bêtement, et de ne pas faire des trucs étranges avec.
+
+Pour discuter, contactez-moi sur discord: Aarnow  
+Serveur discord: https://discord.gg/8j2suEE9Mf
